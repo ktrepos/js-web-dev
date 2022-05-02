ead0ed84a0c615ad9aa65639dc6d7ab536086e96
ktrepos <katialazarova@gmail.com>
2022-05-02 22:48:03 +0200

    Arrays and nested arrays exerc5se

---
+++ 03-js-advanced/js-advanced/06-exercise-arrays-and-nested-arrays/01-print-array-given-delimiter.js
@@ -0,0 +1,18 @@
+function printArray(strArr, delimiter) {
+    console.log(strArr.join(delimiter));
+}
+
+printArray(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-');
+
+printArray(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_');
